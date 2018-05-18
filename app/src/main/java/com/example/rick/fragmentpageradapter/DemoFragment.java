931ae6584c5e6e33139b8d86f4b90156b105b3ef
rick <rick@windscribe.com>
2018-05-18 17:42:58 -0400

    init

---
+++ app/src/main/java/com/example/rick/fragmentpageradapter/DemoFragment.java
@@ -0,0 +1,30 @@
+package com.example.rick.fragmentpageradapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Rick on 2018-05-18.
+ */
+
+
+public class DemoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout resource that'll be returned
+        View rootView = inflater.inflate(R.layout.fragment_demo, container, false);
+
+        // Get the arguments that was supplied when
+        // the fragment was instantiated in the
+        // CustomPagerAdapter
+        Bundle args = getArguments();
+        ((TextView) rootView.findViewById(R.id.text)).setText("Page " + args.getInt("page_position"));
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/rick/fragmentpageradapter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rick.fragmentpageradapter;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    CustomPagerAdapter mCustomPagerAdapter;
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // == Setting up the ViewPager ==
+
+        mCustomPagerAdapter = new CustomPagerAdapter(getFragmentManager(), this);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCustomPagerAdapter);
+    }
+}
